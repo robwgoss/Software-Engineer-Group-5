a79cc68934c40295342a61aefc0d976cb9afb333
GarySingh0915 <singh.gurnaman@gmail.com>
2022-02-24 16:32:48 -0600

    Updated index.js
    added path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 
