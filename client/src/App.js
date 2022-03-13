64add94fab18aa75bbb512be48f03494b90b4570
Gurnaman Singh <singh.gurnaman@gmail.com>
2022-03-13 14:50:53 -0500

    Revert "Codebase Cleanup"

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
 import React, { Fragment } from "react";
 import InputPlayer from "./components/InputPlayer";
+import ListPlayer from "./components/ListPlayer";
 import AppSlashPlayer from "./components/AppSplashPlayer";
 import "./App.css";
+import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Timer from "./components/Timer";
 
+
+
 function App() {
+  
   return (
+    
     <Fragment>
-      <BrowserRouter>
-        <Routes>
+      
+        <BrowserRouter>
+      <Routes>
           <Route path="/" element={<AppSlashPlayer/>}/>
           <Route index element={<AppSlashPlayer/>} />
           <Route path="/entryscreen" element={<InputPlayer />} />
-          <Route path="/playaction" element={<Timer/>} /> 
-        </Routes>
-      </BrowserRouter>
+          <Route path="/playaction" element={<Timer/>} />
+         
+      </Routes>
+    </BrowserRouter>
+    
     </Fragment>
   );
 }
