e91a92a88ec0d65ab37f634a2d3bf4b619abbbdc
GarySingh0915 <singh.gurnaman@gmail.com>
2022-04-01 22:39:50 -0500

    socket port updated
    updated to server

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var server = dgram.createSocket('udp4');
 const WebSocket = require('ws');
 //const wss = new WebSocket.Server({port:8888});
 
-const wss = new WebSocket.Server({server});
+const wss = new WebSocket.Server({port: process.env.PORT});
 
 var dataTransmissionAndHits = [];
 var dataPoints = [];
